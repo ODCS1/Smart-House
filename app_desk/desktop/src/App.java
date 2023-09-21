0c98ea95bdf9409898af18dffc62abb1efc11692
Vitor Coelho <vijcoelho@gmail.com>
2023-09-20 23:55:06 -0300

    modificacao na tela de login, criacao de novas pastas e mudanca nas pastas

--- app_desk/desktop/src/App.java
+++ app_desk/desktop/src/App.java
@@ -1,29 +1,21 @@
+import java.io.IOException;
+
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class App extends Application {
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("TelaLogin.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene tela = new Scene(root);
 
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        tela.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
 
-        Scene scene = new Scene(root, 300, 250);
-
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
+        primaryStage.setTitle("Login");
+        primaryStage.setScene(tela);
         primaryStage.show();
     }
     public static void main(String[] args) {
