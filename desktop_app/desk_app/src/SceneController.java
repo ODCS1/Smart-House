91463c1bc41cfa3e664dc74891d33ccaae0fde4e
Vitor Coelho <130684606+vitorjosecoelho@users.noreply.github.com>
2023-09-30 21:15:33 -0300

    tela Seguranca e mudanca

--- desktop_app/desk_app/src/SceneController.java
+++ desktop_app/desk_app/src/SceneController.java
@@ -59,6 +59,13 @@ public class SceneController {
         stage.setScene(scene);
         stage.show();
     }
+    public void switchToSeg(ActionEvent event) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("TelaSeguranca.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 
     public void login(ActionEvent event) throws IOException {
         String username = usernameField.getText();
