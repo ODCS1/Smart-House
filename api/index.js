a44d84321753a6b590ed6993ddd1f40767c0d5b7
ODCS1 <119748470+ODCS1@users.noreply.github.com>
2023-10-23 12:06:13 -0300

    update API
    Formulário da API e afins

--- api/index.js
+++ api/index.js
@@ -1,70 +1,153 @@
-// AQUI É ONDE OS MODELS SERÃO MANIPULADOS
+const express = require('express')
+const app = express()
+const Sequelize = require('sequelize')
+const handlebars = require('express-handlebars')
+const bodyParser = require('body-parser')
+const porta = 3000
 
-const database = require("./db");
-const Cliente = require("./models/cliente");
-const Pacote = require("./models/pacote");
-const Compra = require("./models/compra");
-const Casa = require("./models/casa");
-const Endereco = require("./models/endereco");
-const Comodo = require("./models/comodo");
-const CasaComodo = require("./models/casaComodo");
-const Led = require("./models/led");
+    // CONFIG
+        // BD
+        const database = require("./db");
+        const Cliente = require("./models/cliente");
+        const Pacote = require("./models/pacote");
+        const Compra = require("./models/compra");
+        const Casa = require("./models/casa");
+        const Endereco = require("./models/endereco");
+        const Comodo = require("./models/comodo");
+        const CasaComodo = require("./models/casaComodo");
+        const Led = require("./models/led");
+        // TEMPLATE ENGINE
+        app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}))
+        app.set('view engine', 'handlebars')
+        // BODY PARSER
+        app.use(bodyParser.urlencoded({extended: false}))
+        app.use(bodyParser.json())
+
+app.get("/", (req,res) => {
+    res.send("<h1>Bem vindo a página principal!</h1>")
+})
+
+app.get("/cadastro", (req,res) => {
+    res.render("cadastro")
+})
+
+app.post("/adicionar", (req,res) => {
+
+    (async () => {
+        await database.sync();
+    
+        // AQUI DENTRO SERÁ SEITO A MANIPULAÇÃO DE DADOS
+    
+        // ----------------------------------------
+        // EXEMPLO DE NOVO CLIENTE
+        // CLIENTE
+        const novoCliente = await Cliente.create({
+            nome_cliente: 'Roberto',
+            sobrenome_cliente: 'Chagas',
+            email_cliente: 'roberto123@gmail.com',
+            senha_cliente: 'rober123',
+            cpf_cliente: 12312345698
+        });
+        console.log(novoCliente);
+    
+        let idCli = await Cliente.findOne({
+            where: id_cliente = 4
+        });
+    
+        let idPacote = 1
+    
+        // COMPRA
+        const novaCompra = await Compra.create({
+            id_cliente: idCli,
+            id_pacote: idPacote
+        });
+    
+        let qtdLed = 4;
+        // CASA
+        const novaCasa = await Casa.create({
+            nome_casa: 'Roberto Casa',
+            qtd_led_casa: qtdLed
+        });
+    
+        const novoCep = 12345789;
+        const novoLogradouro = 'R. Amorim Barbosa';
+        const novoBairro = 'José Fernando';
+        const novoNumero = 543;
+        const novaCidade = 'Maribondo';
+        const novoEstado = 'AL';
+        const novoComplemento = null;
+    
+        const novoEndereco = await Endereco.create({
+            cep: novoCep,
+            logradouro: novoLogradouro,
+            bairro: novoBairro,
+            numero: novoNumero,
+            cidade: novaCidade,
+            estado: novoEstado,
+            complemento: novoComplemento
+        });
+    
+        
+        // ---------------------------------------
+    })();
+})
+
 
 // SINCRONIZAR AQUI COM O BD
-(async () => {
-    await database.sync();
+// (async () => {
+//     await database.sync();
 
-    // AQUI DENTRO SERÁ SEITO A MANIPULAÇÃO DE DADOS
+//     // AQUI DENTRO SERÁ SEITO A MANIPULAÇÃO DE DADOS
 
-    // ----------------------------------------
-    // EXEMPLO DE NOVO CLIENTE
-    // CLIENTE
-    const novoCliente = await Cliente.create({
-        nome_cliente: 'Roberto',
-        sobrenome_cliente: 'Chagas',
-        email_cliente: 'roberto123@gmail.com',
-        senha_cliente: 'rober123',
-        cpf_cliente: 12312345698
-    });
-    console.log(novoCliente);
+//     // ----------------------------------------
+//     // EXEMPLO DE NOVO CLIENTE
+//     // CLIENTE
+//     const novoCliente = await Cliente.create({
+//         nome_cliente: 'Roberto',
+//         sobrenome_cliente: 'Chagas',
+//         email_cliente: 'roberto123@gmail.com',
+//         senha_cliente: 'rober123',
+//         cpf_cliente: 12312345698
+//     });
+//     console.log(novoCliente);
 
-    let idCli = await Cliente.findOne({
-        where: id_cliente = 4
-    });
+//     let idCli = await Cliente.findOne({
+//         where: id_cliente = 4
+//     });
 
-    let idPacote = 1
+//     let idPacote = 1
 
-    // COMPRA
-    const novaCompra = await Compra.create({
-        id_cliente: idCli,
-        id_pacote: idPacote
-    });
+//     // COMPRA
+//     const novaCompra = await Compra.create({
+//         id_cliente: idCli,
+//         id_pacote: idPacote
+//     });
 
-    let qtdLed = 4;
-    // CASA
-    const novaCasa = await Casa.create({
-        nome_casa: 'Roberto Casa',
-        qtd_led_casa: qtdLed
-    });
+//     let qtdLed = 4;
+//     // CASA
+//     const novaCasa = await Casa.create({
+//         nome_casa: 'Roberto Casa',
+//         qtd_led_casa: qtdLed
+//     });
 
-    const novoCep = 12345789;
-    const novoLogradouro = 'R. Amorim Barbosa';
-    const novoBairro = 'José Fernando';
-    const novoNumero = 543;
-    const novaCidade = 'Maribondo';
-    const novoEstado = 'AL';
-    const novoComplemento = null;
+//     const novoCep = 12345789;
+//     const novoLogradouro = 'R. Amorim Barbosa';
+//     const novoBairro = 'José Fernando';
+//     const novoNumero = 543;
+//     const novaCidade = 'Maribondo';
+//     const novoEstado = 'AL';
+//     const novoComplemento = null;
 
-    const novoEndereco = await Endereco.create({
-        cep: novoCep,
-        logradouro: novoLogradouro,
-        bairro: novoBairro,
-        numero: novoNumero,
-        cidade: novaCidade,
-        estado: novoEstado,
-        complemento: novoComplemento
-    });
+//     const novoEndereco = await Endereco.create({
+//         cep: novoCep,
+//         logradouro: novoLogradouro,
+//         bairro: novoBairro,
+//         numero: novoNumero,
+//         cidade: novaCidade,
+//         estado: novoEstado,
+//         complemento: novoComplemento
+//     });
 
     
-    // ---------------------------------------
-})();
+//     // ---------------------------------------
+// })();
