b8f80f42432ed86b80a0a321e041754b46b80158
Vitor Coelho <130684606+vitorjosecoelho@users.noreply.github.com>
2023-10-30 08:31:10 -0300

    Lampadas Atualizadas
    As lampadas no javaFx estao funcionando ligando e desligando perfeitamente. Proximo passo é colocar o banco de dados nelas

---
+++ desktop_app/desk_app/src/BanheiroController.java
@@ -0,0 +1,112 @@
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+public class BanheiroController {
+
+    @FXML
+    private CheckBox myCheckBox;
+    @FXML
+    private Label myLabel;
+    @FXML
+    private ImageView myImageView;
+
+    private Stage stage;
+    private Scene scene;
+
+
+    Image myImage1 = new Image(getClass().getResourceAsStream("./img/light-bulb.png"));
+    Image myImage2 = new Image(getClass().getResourceAsStream("./img/light-bulb-on.png"));
+
+    @FXML
+    public void change(ActionEvent event) {
+        if (myCheckBox.isSelected()) {
+            myLabel.setText("ON");
+            myImageView.setImage(myImage2);
+        } else {    
+            myLabel.setText("OFF");
+            myImageView.setImage(myImage1);
+        }
+    }
+
+    public void switchToScene1(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("./telas/TelaLogin.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToScene2(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("./telas/TelaMain.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void MostrarLuzSala(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("./telas/TelaSala.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void MostrarLuzMaster(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("./telas/TelaMaster.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void MostrarLuzJardim(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("./telas/TelaJardim.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void MostrarLuzQuarto1(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("./telas/TelaQuarto1.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void MostrarLuzQuarto2(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("./telas/TelaQuarto2.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToSeg(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("./telas/TelaSeguranca.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void MostrarLuzBanheiro(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("./telas/TelaBanheiro.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }   
+}
