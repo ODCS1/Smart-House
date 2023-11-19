73dcb9decdc587a29a883d74dd3531f1ec860a61
Vitor Coelho <130684606+vitorjosecoelho@users.noreply.github.com>
2023-11-19 20:16:24 -0300

    Mudei o select * para um Procedure na parte de autenticacao do usuario

--- desktop_app/desk_app/src/dao/UsuarioDAO.java
+++ desktop_app/desk_app/src/dao/UsuarioDAO.java
@@ -1,48 +1,39 @@
 package dao;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import com.mysql.cj.jdbc.CallableStatement;
 import conexao.Conexao;
 import entidade.Usuario;
 
 public class UsuarioDAO {
 
-    public PreparedStatement ps;
-    public ResultSet rs;
-    public String sql;
-    
     public Usuario verificarCredenciais(Usuario usuario) {
-        Connection connection = Conexao.getConnection();
-        // Conexao com o banco de dados
-
+        Connection conn = Conexao.getConnection();
         Usuario usuarioCompleto = null;
     
         try {
-            sql = "SELECT * FROM sistema_casa.Clientes WHERE email_cliente = ? AND senha_cliente = ?";
-            ps = connection.prepareStatement(sql);
-            ps.setString(1, usuario.getEmail_cliente());
-            ps.setString(2, usuario.getSenha_cliente());
+            String callProcedure = "{CALL autenticar_usuario(?, ?)}";
+            CallableStatement cs = (CallableStatement) conn.prepareCall(callProcedure);
+            cs.setString(1, usuario.getEmail_cliente());
+            cs.setString(2, usuario.getSenha_cliente());
 
-            // Usa o SELECT para pesquisar na tabela de usuario se existe alguem com 
-            // o email e senha digitado para logar
-    
-            rs = ps.executeQuery();
-    
+            java.sql.ResultSet rs = cs.executeQuery();
+
             if (rs.next()) {
                 usuarioCompleto = new Usuario();
                 usuarioCompleto.setNome_cliente(rs.getString("nome_cliente"));
                 usuarioCompleto.setSobrenome_cliente(rs.getString("sobrenome_cliente"));
                 usuarioCompleto.setEmail_cliente(rs.getString("email_cliente"));
                 usuarioCompleto.setCpf_cliente(rs.getString("cpf_cliente"));
-
-                //Seto o nome, email e cpf das respectivas colunas
+                
+                return usuarioCompleto;
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return usuarioCompleto;
+
+        return null;
     }
 }
